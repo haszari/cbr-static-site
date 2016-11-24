95abf8f4371445577da9e4c1fda0b8cf24b155a8
haszari <haszari@cartoonbeats.com>
2016-11-24 23:40:01 +1300

    webpackify ready for reactification

---
+++ src/entry.jsx
@@ -0,0 +1,18 @@
+import './css/foundation.css';
+import './css/app.css';
+
+// document.write("It works SWEET.")
+
+// import React from 'react';
+// import {render} from 'react-dom';
+
+// class App extends React.Component {
+//   render () {
+//     return <p> Hello React!</p>;
+//   }
+// }
+
+// var appDiv = document.createElement('div');
+// document.body.appendChild(appDiv);
+
+// render(<App/>, appDiv);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: "./src/entry.jsx",
+   output: {
+      path: __dirname + "/dist/",
+      filename: "bundle.js"
+   },
+   devtool: 'source-map',
+   module: {
+      loaders: [
+         { 
+            test: /\.jsx?$/, 
+            exclude: /node_modules/, 
+            loader: "babel",
+            query: {
+               presets: ['es2015', 'react']
+            },
+         },
+         { 
+            test: /\.css$/, 
+            loader: "style!css" 
+         },
+         {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+         },
+         {
+            test: /\.html$/,
+            loader: 'html-loader'
+         },
+         {
+            test: [/\.jpg$/, /\.svg$/, /\.png$/],
+            loader: 'file-loader'
+         }
+      ]
+   },
+   // this helps generate an html file for our generated bundle filename
+   plugins: [new HtmlWebpackPlugin({
+      template: 'src/index.html'      
+   })]
+};
