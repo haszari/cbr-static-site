3863f5b15397d80565e62524906164c17c95d091
haszari <haszari@cartoonbeats.com>
2016-11-28 19:04:05 +1300

    release component renders remixers generically

--- src/components/release-list.jsx
+++ src/components/release-list.jsx
@@ -3,13 +3,48 @@ import cbrReleaseInfo from '../data/cartoonbeats-releases';
 
 
 class ReleaseItem extends React.Component {
+   rowClasses() {
+      const classes = ["row", "release"];
+      classes.push(`release-${this.props.catalogueNumber}`);
+      classes.push(this.props.leftText ? "release-leftText" : "release-rightText");
+      return classes.join(' ');
+   }
+
+   renderRemixers() {
+      let remixersString = "";
+      let remixers = this.props.remixers;
+      if (remixers && remixers.length) {
+         remixersString = remixers.pop();
+
+         if (remixers.length) 
+            remixersString = `${remixers.pop()} & ${remixersString}`;
+
+         while (remixers.length)
+            remixersString = `${remixers.pop()}, ${remixersString}`;
+
+         remixersString = (<div><i>+ remixes by</i> {remixersString}</div>);
+      }
+      
+      return remixersString;
+   }
+
    render () {
       return (
-         <div className="row release">
-            <div className="small-12 columns column-info">
-               <div>
-               {this.props.catalogueNumber}
+         <div className={this.rowClasses()}>
+            <div className="small-5 columns column-info">
+               <div className="release-title">
+               {this.props.title}
                </div>
+               <div className="release-artists">
+                  {this.props.artist}
+               </div>
+               <div className="release-remixers">
+                  {this.renderRemixers()}
+               </div>
+            </div>
+            <div className="small-7 columns column-coverart">
+               <img className="release-coverart" src="img/releases/CBR004A-front.jpg">
+               </img>
             </div>
          </div>
       );
@@ -18,8 +53,8 @@ class ReleaseItem extends React.Component {
 
 export default class ReleaseList extends React.Component {
    render () {
-      const releaseList = cbrReleaseInfo.map((release) =>
-         <ReleaseItem catalogueNumber={release.catalogueNumber} key={release.catalogueNumber}></ReleaseItem>
+      const releaseList = cbrReleaseInfo.map((release, i) =>
+         <ReleaseItem {... release} leftText={(i % 2) == 0} key={release.catalogueNumber} />
       );
       return <div>{releaseList}</div>;
   }
--- src/data/cartoonbeats-releases.js
+++ src/data/cartoonbeats-releases.js
@@ -1,8 +1,20 @@
 export default [
    {
       catalogueNumber: "cbr004a",
+      title: "Blood Orange",
+      artist: "Haszari",
+      remixers: [
+      	"SoNic Smith",
+      	"nsu"
+      ]
    },
    {
       catalogueNumber: "cbr004b",
-   },
+      title: "Re: Memory",
+      artist: "SoNic Smith",
+      remixers: [
+      	"Haszari",
+      	"nsu"
+      ]
+   }
 ];
