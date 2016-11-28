4394fe5c4b1257203324da3eec3329ed27a0d4b5
haszari <haszari@cartoonbeats.com>
2016-11-28 23:03:40 +1300

    setting up react releases data - up to 003

--- src/data/cartoonbeats-releases.js
+++ src/data/cartoonbeats-releases.js
@@ -1,6 +1,15 @@
 import cbr004aImage from '../img/releases/CBR004A-front.jpg';
 import cbr004bImage from '../img/releases/CBR004B-front.jpg';
 
+import cbr003aImage from '../img/releases/CBR003A-front.jpg';
+import cbr003bImage from '../img/releases/CBR003B-front.jpg';
+
+import cbr002aImage from '../img/releases/CBR002A-front.jpg';
+import cbr002bImage from '../img/releases/CBR002B-front.jpg';
+
+import cbr001aImage from '../img/releases/CBR001A-front.jpg';
+import cbr001bImage from '../img/releases/CBR001B-front.jpg';
+
 export default [
    {
       catalogueNumber: "cbr004a",
@@ -23,7 +32,7 @@ export default [
    },
    {
       catalogueNumber: "cbr004b",
-      title: "Re: Memory",
+      title: "RE: Memory",
       artist: "SoNic Smith",
       image: cbr004bImage,
       remixers: [
@@ -39,5 +48,134 @@ export default [
       	googlePlay: 'https://play.google.com/store/music/album/SoNic_Smith_RE_Memory?id=Bczniwskxl4if6pmbml22ge6fsa',
       	amazon: 'http://www.amazon.com/gp/product/B01N04D2Z0'
       }
-   }
+   },
+
+   {
+      catalogueNumber: "cbr003a",
+      title: "It's Not Real",
+      artist: "Haszari",
+      image: cbr003aImage,
+      remixers: [
+      	"nsu",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#051966",
+      },
+      listenLinks: {
+      	spotify: 'https://open.spotify.com/album/63YdghfUuwS4BfIdVTQD9d',
+      	appleMusic: 'https://itunes.apple.com/us/album/its-not-real-ep/id1175884694?uo=4&app=music',
+      	googlePlay: 'https://play.google.com/store/music/album/Haszari_It_s_Not_Real?id=Bzf73nrzcmcwcpmugbcvd3ys7oi',
+      	amazon: 'http://www.amazon.com/gp/product/B01N3RF0T7'
+      }
+   },
+   {
+      catalogueNumber: "cbr003b",
+      title: "Neutron Star MMX",
+      artist: "nsu",
+      image: cbr003bImage,
+      remixers: [
+      	"Haszari",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#f75c21",
+      },
+      listenLinks: {
+      	spotify: 'https://open.spotify.com/album/64BBGrJVJJ68tmiFWj4syz',
+      	appleMusic: 'https://itunes.apple.com/us/album/neutron-star-mmx-ep/id1175890606?uo=4&app=music',
+      	googlePlay: 'https://play.google.com/store/music/album/Nsu_Neutron_Star_MMX?id=Bnhg26amosrjlooi4v2ide72yyq',
+      	amazon: 'http://www.amazon.com/gp/product/B01N04D5JD'
+      }
+   },
+
+   {
+      catalogueNumber: "cbr003a",
+      title: "It's Not Real",
+      artist: "Haszari",
+      image: cbr003aImage,
+      remixers: [
+      	"nsu",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#051966",
+      },
+      listenLinks: {
+      	spotify: '',
+      	appleMusic: '',
+      	googlePlay: '',
+      	amazon: ''
+      }
+   },
+   {
+      catalogueNumber: "cbr003b",
+      title: "Neutron Star MMX",
+      artist: "nsu",
+      image: cbr003bImage,
+      remixers: [
+      	"Haszari",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#f75c21",
+      },
+      listenLinks: {
+      	spotify: '',
+      	appleMusic: '',
+      	googlePlay: '',
+      	amazon: ''
+      }
+   },
+
+   {
+      catalogueNumber: "cbr003a",
+      title: "It's Not Real",
+      artist: "Haszari",
+      image: cbr003aImage,
+      remixers: [
+      	"nsu",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#051966",
+      },
+      listenLinks: {
+      	spotify: '',
+      	appleMusic: '',
+      	googlePlay: '',
+      	amazon: ''
+      }
+   },
+   {
+      catalogueNumber: "cbr003b",
+      title: "Neutron Star MMX",
+      artist: "nsu",
+      image: cbr003bImage,
+      remixers: [
+      	"Haszari",
+      	"Aural Trash", 
+      	"Snake Charmer",
+      	"Greg Churchill"
+      ],
+      colours: {
+      	background: "#f75c21",
+      },
+      listenLinks: {
+      	spotify: '',
+      	appleMusic: '',
+      	googlePlay: '',
+      	amazon: ''
+      }
+   },
 ];
